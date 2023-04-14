d3f933731e39da3de428b6d40ec0ef06a9ee6094
Devan Rodabough <devan_rodabough@laurel.k12.mt.us>
2023-04-14 17:27:15 -0600

    Cleaned up file tree and made a locator script.

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
-elemDistrictName = 'LAUREL ELEMENTARY'
-secondaryDistrictName = 'LAUREL HIGH SCHOOL'
-shapefilePrimary = 'MontanaSchoolDistricts_shp/Elementary.shp'
-shapefileSecondary = 'MontanaSchoolDistricts_shp/secondary/Secondary.shp'
-shapeFaceColor = 'gold'
+import os
+
+# Name of Districts according to data on the files
+elemDistrictName = 'ELDER GROVE ELEMENTARY'
+secondaryDistrictName = None
+# Shapefile path
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is root of github project
+SHAPEFILES_DIR = os.path.join(ROOT_DIR, 'MontanaSchoolDistricts_shp')
+shapefilePrimary = os.path.join(SHAPEFILES_DIR, 'Elementary.shp')
+shapefileSecondary = os.path.join(SHAPEFILES_DIR, 'secondary', 'Secondary.shp')
+
+
+# Set Map Properties #
 shapeEdgeColor = 'black'
+shapeFaceColor = 'gold'
 mapZoomLevel = 15
 imageDPI = 300
--- mapping.py
+++ mapping.py
@@ -7,24 +7,25 @@ import matplotlib.image as mpimg
 from config import shapefilePrimary, shapefileSecondary, elemDistrictName, secondaryDistrictName, shapeEdgeColor, shapeFaceColor, mapZoomLevel, imageDPI
 
 # read in the first shapefile
-shapes1 = gpd.read_file(shapefilePrimary)
-elementaryDistrict = shapes1.loc[shapes1['NAME'] == elemDistrictName]
+if elemDistrictName is not None:
+    shapes1 = gpd.read_file(shapefilePrimary)
+    elementaryDistrict = shapes1.loc[shapes1['NAME'] == elemDistrictName]
 
 # read in the second shapefile
-shapes2 = gpd.read_file(shapefileSecondary)
-highschoolDistrict = shapes2.loc[shapes2['NAME'] == secondaryDistrictName]
+if secondaryDistrictName is not None:
+    shapes2 = gpd.read_file(shapefileSecondary)
+    highschoolDistrict = shapes2.loc[shapes2['NAME'] == secondaryDistrictName]
 
 # create a plot with a basemap
 fig, ax = plt.subplots(figsize=(72, 60))
 elementaryDistrict.plot(ax=ax, facecolor=shapeFaceColor, alpha=.25, zorder=1, edgecolor=shapeEdgeColor)
 highschoolDistrict.plot(ax=ax, facecolor=shapeFaceColor, alpha=.15, zorder=2, edgecolor=shapeEdgeColor)
-ctx.add_basemap(ax, crs=elemDistrictName.crs, zoom=mapZoomLevel, source=ctx.providers.OpenStreetMap.Mapnik)
+ctx.add_basemap(ax, crs=elementaryDistrict.crs, zoom=mapZoomLevel, source=ctx.providers.OpenStreetMap.Mapnik)
 
-# load the image
-img = mpimg.imread('my_map.png')
+# apply interpolation to the basemap image
+for img in ax.get_images():
+    img.set_interpolation('bicubic')
 
-# plot the image with interpolation
-ax.imshow(img, extent=[-109.66, -109.52, 45.88, 45.98], interpolation='bicubic')
 
 # remove the axis
 ax.set_axis_off()
