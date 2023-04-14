16b0a71e1a8d951373229d7035db31155b4a471a
Devan Rodabough <devan_rodabough@laurel.k12.mt.us>
2023-04-14 14:19:37 -0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+elemDistrictName = 'LAUREL ELEMENTARY'
+secondaryDistrictName = 'LAUREL HIGH SCHOOL'
+shapefilePrimary = 'MontanaSchoolDistricts_shp/Elementary.shp'
+shapefileSecondary = 'MontanaSchoolDistricts_shp/secondary/Secondary.shp'
+shapeFaceColor = 'gold'
+shapeEdgeColor = 'black'
+mapZoomLevel = 15
+imageDPI = 300
