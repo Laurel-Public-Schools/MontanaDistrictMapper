3f49717fafa6da4aee4499b07d8deff35aaf4f2c
Devan Rodabough <devan_rodabough@laurel.k12.mt.us>
2023-04-17 16:47:15 -0600

    fixed commit and fixed map image. Added map image to readme

--- alldistrictmaps.py
+++ alldistrictmaps.py
@@ -15,16 +15,21 @@ mapsize_inches_y = 30
 primary_schools = gpd.read_file(shapefilePrimary)
 secondary_schools = gpd.read_file(shapefileSecondary)
 unified_districts = gpd.read_file(shapefileUnified)
-all_districts = primary_schools._append(secondary_schools).append(shapefileUnified)
+all_districts = (primary_schools._append(secondary_schools))._append(unified_districts)
 
 # Plot primary school districts
 fig, ax = plt.subplots(figsize=(mapsize_inches_x, mapsize_inches_y))
 all_districts.plot(ax=ax,  alpha=.25, zorder=1, edgecolor=shapeEdgeColor)
-ax.set_title('Montana Primary School Districts')
+ax.set_title('Montana School Districts')
 ax.set_xlim(all_districts.total_bounds[0], all_districts.total_bounds[2])
 ax.set_ylim(all_districts.total_bounds[1], all_districts.total_bounds[3])
 ax.set_aspect('equal')
+# remove the axis
+ax.set_axis_off()
+
+for img in ax.get_images():
+    img.set_interpolation('bicubic')
 
 ctx.add_basemap(ax, zoom=mapZoomLevel, source=ctx.providers.OpenStreetMap.Mapnik, crs=secondary_schools.crs)
-plt.show()
 plt.savefig('AllDistricts.png', dpi=imageDPI)
+#plt.show()
